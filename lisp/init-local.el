0d3763374afa63ac21b544e1eb11a09b90850489
xfavor <15181653622@163.com>
2019-04-14 15:29:50 +0800

    add config_list and install org2blog

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,23 +1,23 @@
 ;;; commentary:
 ;;; code:
 
-;; Temporary configuration ;;
-;; define a function, press F1 quickly open your config file
+;;; Temporary configuration ;;
+;;; define a function, press F1 quickly open your config file
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 (global-set-key (kbd "<f1>") 'open-my-init-file)
 
+;;; press F2 come back here.
 (defun open-my-local-file()
   (interactive)
   (find-file "~/.emacs.d/lisp/init-local.el"))
 (global-set-key (kbd "<f2>") 'open-my-local-file)
 
 
-;; config neotree
+;;; config neotree
 (require 'neotree)
 (global-set-key [f8] 'neotree)
-
 (setq neo-smart-open t)
 
 
@@ -28,6 +28,7 @@
              ad-do-it)))
   )
 
+;;; Support Java language
 (require 'meghanada)
 (add-hook 'java-mode-hook
           (lambda ()
@@ -45,13 +46,20 @@
   (setq meghanada-java-path "java")
   (setq meghanada-maven-path "mvn")))
 
+;; Support org output to pdf
 (setq org-latex-pdf-process '("xelatex -interaction nonstopmode %f"
                               "xelatex -interaction nonstopmode %f"
                               ))
 
-;;(setq org-latex-default-packages-alist      (remove '("AUTO" "inputenc" t) org-latex-default-packages-alist))
+
+;;; Support org-mind-map, but here is a bug, I suggest that turn on org-mind-map when you want to use.
+;;(load "org-mind-map.el")
 
 
-(load "org-mind-map.el")
+;;; Support org to wordpress
+
+
+;;; Support
+
 
 (provide 'init-local)
